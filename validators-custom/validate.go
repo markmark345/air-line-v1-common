f86ebcd79ea483e56fe09785e9e2cb961e019e5b
Natapatchara <natapatchara.anuroje@scbtechx.io>
2023-08-02 11:43:31 +0700

    Chang type validate err

--- validators-custom/validate.go
+++ validators-custom/validate.go
@@ -13,25 +13,25 @@ func ValidateCustomError(ctx *gin.Context, err error) {
 		for _, err := range err.(validator.ValidationErrors) {
 			switch err.Tag() {
 			case "required":
-				responses.Error(ctx, errors.New("Missing required field "+"\""+err.Field()+"\""))
+				responses.Failure(ctx, "invaild_parameters", errors.New("Missing required field "+"\""+err.Field()+"\""))
 				return
 			case "email":
-				responses.Error(ctx, errors.New("Invalid email format for field "+"\""+err.Field()+"\""))
+				responses.Failure(ctx, "invaild_parameters", errors.New("Invalid email format for field "+"\""+err.Field()+"\""))
 				return
 			case "max":
-				responses.Error(ctx, errors.New("Value for field \""+err.Field()+"\" exceeds the maximum allowed"))
+				responses.Failure(ctx, "invaild_parameters", errors.New("Value for field \""+err.Field()+"\" exceeds the maximum allowed"))
 				return
 			case "min":
-				responses.Error(ctx, errors.New("Value for field \""+err.Field()+"\" does not meet the minimum required"))
+				responses.Failure(ctx, "invaild_parameters", errors.New("Value for field \""+err.Field()+"\" does not meet the minimum required"))
 				return
 			case "boolean":
-				responses.Error(ctx, errors.New("Invalid value for boolean field \""+err.Field()+"\""))
+				responses.Failure(ctx, "invaild_parameters", errors.New("Invalid value for boolean field \""+err.Field()+"\""))
 				return
 			case "len":
-				responses.Error(ctx, errors.New("Invalid length for field \""+err.Field()+"\""))
+				responses.Failure(ctx, "invaild_parameters", errors.New("Invalid length for field \""+err.Field()+"\""))
 				return
 			case "oneof":
-				responses.Error(ctx, errors.New("Invalid value for field \""+err.Field()+"\". Allowed values are M, N, or W"))
+				responses.Failure(ctx, "invaild_parameters", errors.New("Invalid value for field \""+err.Field()+"\". Allowed values are M, N, or W"))
 				return
 			}
 		}
